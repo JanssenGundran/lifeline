2a992e8179c343532fe86acdf77182af49b7b00a
jamg <gundranjanjan@gmail.com>
2025-03-22 11:59:29 +0800

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,13 @@ require('dotenv').config();
 const app = express();
 connectDB();
 
+app.use(cors({
+  origin: 'http://localhost:4200',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
-app.use(cors());
 
 app.use('/api/jobs', require('./routes/jobRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
