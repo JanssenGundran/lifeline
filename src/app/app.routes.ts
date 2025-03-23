e15e14aba8cda0f63304f3b161ac58bc9ab3bf79
jsimbul56 <51770670+jsimbul56@users.noreply.github.com>
2025-03-23 15:24:51 +0000

    Pending changes exported from your codespace

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { JobBoardComponent } from './job-board/job-board.component';
 import { JobManagementComponent } from './job-management/job-management.component';
 import { ApplyComponent } from './apply/apply.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
   { path: 'jobs', component: JobBoardComponent },
   { path: 'manage-jobs', component: JobManagementComponent },
   { path: 'apply/:id', component: ApplyComponent },
+  { path: 'contacts', component: ContactComponent },
 ];
